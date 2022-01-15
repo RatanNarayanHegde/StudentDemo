95ac8ebfa950125e1019fd771248b843e629a166
Ratan Hegde <ratanhegde@pop-os.localdomain>
2022-01-15 22:26:26 +0530

    add ant design

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
+import { Button } from 'antd';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello React
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button type="primary">Button</Button>
     </div>
   );
 }
